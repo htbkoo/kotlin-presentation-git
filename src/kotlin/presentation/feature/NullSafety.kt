8550036115cb273ef043247b5d09ca9a6408ab34
Hey <hey20081234@gmail.com>
2017-03-16 00:26:55 +0800

    Initial commit - creation of the Kotlin-presenation project

---
+++ src/kotlin/presentation/feature/NullSafety.kt
@@ -0,0 +1,10 @@
+package kotlin.presentation.feature
+
+/**
+ * Created by Hei on 16/03/2017.
+ */
+
+class NullSafety {
+    val nullableString: String? = null;
+    val nonNullableString: String = "";
+}
